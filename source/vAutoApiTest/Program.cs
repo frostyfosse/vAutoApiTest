ab54f1a01be672f95a6403e17a2e5643fb46c26a
Fosse <frostyfosse@gmail.com>
2018-04-02 01:44:03 -0400

    Full Solution
    Apparently my original drop of all files were not accepted when I
    reverted (oops). Including them all again.

---
+++ source/vAutoApiTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace vAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new Processor();
+
+            try
+            {
+                processor.Process();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
